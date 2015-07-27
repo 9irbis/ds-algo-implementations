23d9230f5f60ed0bf5f9d716b1580832fc22173f
Nakul <9irbis@gmail.com>
2015-07-27 12:52:47 +0530

    added list empty exception class

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -5,49 +5,66 @@
 
 using namespace std;
 
-/* change the following type definition to get a doubly linked list holding 
-   some type other than string. I Could have used templated class to implement 
-   a truly generic list but I wanted to avoid the notational overhead.
-*/
-typedef string Elem;
+class RuntimeException{    //  general base exception class
+private:
+    string errMsg;
+public:
+    RuntimeException(const string& err): errMsg(err) {}
+    string getMessage() const {return errMsg;}
+};
 
+class ListEmpty: public RuntimeException{   // exception to throw when trying to
+                                            // access or remove an item in an empty list
+public:
+    ListEmpty(const string& s): RuntimeException(s) {}
+};
+
+template <typename E>  // forward declaration of linked list class
+class DLinkedList;
+
+template <typename E>
 class DNode{        // class representing an individual node of the list
 private:        
-    Elem elem;      // actual element stored at this node
-    DNode* prev;    // pointer to previous node
-    DNode* next;    // pointer to next node
-    friend class DLinkedList;  // declare list class as friend for private member
+    E elem;      // actual element stored at this node
+    DNode<E>* prev;    // pointer to previous node
+    DNode<E>* next;    // pointer to next node
+    friend class DLinkedList<E>;  // declare list class as friend for private member
                                // access
 };
 
+
+template <typename E>
 class DLinkedList{     // class representing a doubly linked list 
 private:               
-    DNode* header;     // pointer to header dummy node used to simplify operations
-    DNode* trailer;    // pointer to trailer dummy node used to simplify operations
+    DNode<E>* header;     // pointer to header dummy node used to simplify operations
+    DNode<E>* trailer;    // pointer to trailer dummy node used to simplify operations
 public:
     DLinkedList();
     ~DLinkedList();
     bool empty() const;           // return true if list is empty
-    const Elem& front() const;    // return value stored at front node
-    const Elem& back() const;     // return value stored at back node
-    void addFront(const Elem& e);
-    void addBack(const Elem& e);
-    void removeFront();
-    void removeBack();
+    const E& front() const throw(ListEmpty);    // return value stored at front node
+    const E& back() const throw(ListEmpty);     // return value stored at back node
+    void addFront(const E& e);
+    void addBack(const E& e);
+    void removeFront() throw(ListEmpty);
+    void removeBack() throw(ListEmpty);
 protected:                       // protected methods for internal use
-    void add(DNode* v, const Elem& e);  // add e before node v
-    void remove(DNode* v);              // remove node v
+    void add(DNode<E>* v, const E& e);  // add e before node v
+    void remove(DNode<E>* v);              // remove node v
 };
 
-DLinkedList::DLinkedList()
+
+template <typename E>
+DLinkedList<E>::DLinkedList()
 {
-    header = new DNode;
-    trailer = new DNode;
+    header = new DNode<E>;
+    trailer = new DNode<E>;
     header->next = trailer;
     trailer->prev = header;
 }
 
-DLinkedList::~DLinkedList()
+template <typename E>
+DLinkedList<E>::~DLinkedList()
 {
     while(!empty())
         removeFront();
@@ -55,63 +72,80 @@ DLinkedList::~DLinkedList()
     delete trailer;
 }
 
-bool DLinkedList::empty() const
+template <typename E>
+bool DLinkedList<E>::empty() const
 {
     return (header->next == trailer);    // If there are no nodes b/w header and
                                          // trailer, the list is empty
 }
 
-const Elem& DLinkedList::front() const
+template <typename E>
+const E& DLinkedList<E>::front() const throw(ListEmpty)
 {
+    if (empty())
+        throw ListEmpty("List is empty!");
     return header->next->elem;
 }
 
-const Elem& DLinkedList::back() const
-{
+template <typename E>
+const E& DLinkedList<E>::back() const throw(ListEmpty)
+{  
+    if (empty())
+        throw ListEmpty("List is empty!");
     return trailer->prev->elem;
 }
 
-void DLinkedList::add(DNode* v, const Elem& e)  // add e before node v
+template <typename E>
+void DLinkedList<E>::add(DNode<E>* v, const E& e)  // add e before node v
 {
-    DNode* u = new DNode;
+    DNode<E>* u = new DNode<E>;
     u->elem = e;
     u->next = v;
     u->prev = v->prev;
     v->prev->next = v->prev = u;
 }
 
-void DLinkedList::addFront(const Elem& e)
+template <typename E>
+void DLinkedList<E>::addFront(const E& e)
 {
     add(header->next, e);
 }
 
-void DLinkedList::addBack(const Elem& e)
+template <typename E>
+void DLinkedList<E>::addBack(const E& e)
 {
     add(trailer, e);
 }
 
-void DLinkedList::remove(DNode* v)      // remove node pointed by v
+template <typename E>
+void DLinkedList<E>::remove(DNode<E>* v)      // remove node pointed by v
 {
-    DNode* u = v->prev;
-    DNode* w = v->next;
+    DNode<E>* u = v->prev;
+    DNode<E>* w = v->next;
     u->next = w;
     w->prev = u;
     delete v;
 }
 
-void DLinkedList::removeFront()
+template <typename E>
+void DLinkedList<E>::removeFront() throw(ListEmpty)
 {
+    if (empty())
+        throw ListEmpty("List is empty!");
     remove(header->next);
 }
 
-void DLinkedList::removeBack()
+template <typename E>
+void DLinkedList<E>::removeBack() throw(ListEmpty)
 {
+    if (empty())
+        throw ListEmpty("List is empty!");
     remove(trailer->prev);
 }
 
 int main()
 {
-    DLinkedList legends;
+    DLinkedList<string> legends;
     legends.addFront("Pele");
     legends.addFront("Maradona");
     legends.addBack("Messi");
